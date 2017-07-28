5650244e2cf63552d9411feaa9c3ac0448e8e9bd
Stefan Skliarov <serg.osipchuk@rapidapi.com>
2017-07-28 18:22:49 +0300

    access token fixed

--- src/routes/getAccessToken.php
+++ src/routes/getAccessToken.php
@@ -1,3 +1,4 @@
+
 <?php
 $app->post('/api/OutlookMail/getAccessToken', function ($request, $response, $args) {
     $settings = $this->settings;
@@ -11,16 +12,52 @@ $app->post('/api/OutlookMail/getAccessToken', function ($request, $response, $ar
         $post_data = $validateRes;
     }
     //forming request to vendor API
+    //forming request to vendor API
     $query_str ='https://login.microsoftonline.com/common/oauth2/v2.0/token';
-    $post_data['args']['grant_type'] = 'authorization_code';
-    $params = [
-        'client_id' => 'clientId',
-        'client_secret' => 'clientSecret',
-        'code'=> 'code',
-        'redirect_uri'=> 'redirectUri',
-        'grant_type'=> 'grant_type'
-    ];
-    $result = \Models\ApiRequestFacade::makeRequest($params, $post_data, $query_str, 'POST', 'json');
+    $body['client_id'] = $post_data['args']['clientId'];
+    $body['client_secret'] = $post_data['args']['clientSecret'];
+    $body['code'] = $post_data['args']['code'];
+    $body['redirect_uri'] = $post_data['args']['redirectUri'];
+    $body['grant_type'] = 'authorization_code';
+    $client = new \GuzzleHttp\Client();
+
+    try {
+        $resp = $client->request('POST', $query_str, [
+            'form_params' => $body
+        ]);
+
+        $responseBody = $resp->getBody()->getContents();
+        $rawBody = json_decode($resp->getBody());
+        $all_data[] = $rawBody;
+        if ($resp->getStatusCode() == '200') {
+            $result['callback'] = 'success';
+            $result['contextWrites']['to'] = is_array($all_data) ? $all_data : json_decode($all_data);
+        } else {
+            $result['callback'] = 'error';
+            $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+            $result['contextWrites']['to']['status_msg'] = is_array($responseBody) ? $responseBody : json_decode($responseBody);
+        }
+
+    } catch (\GuzzleHttp\Exception\ClientException $exception) {
+        $responseBody=$exception->getResponse()->getBody()->getContents();
+        $result['callback'] = 'error';
+        $result['contextWrites']['to']['status_code'] = 'API_ERROR';
+        $result['contextWrites']['to']['status_msg'] = $responseBody;
+
+    } catch (ServerException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to'] = json_decode($responseBody);
+
+    } catch (BadResponseException $exception) {
+
+        $responseBody = $exception->getResponse()->getBody(true);
+        $result['callback'] = 'error';
+        $result['contextWrites']['to'] = json_decode($responseBody);
+
+    }
+
     return $response->withHeader('Content-type', 'application/json')->withStatus(200)->withJson($result);
 
 });
